bca42f1c397f98671f3e308e4b533906c26bdbf3
Anna <67719118+brasspetals@users.noreply.github.com>
2022-04-07 13:50:15 -0500

    Add base files and mobile header

---
+++ src/components/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Nav from './Nav.svelte'
+</script>
+
+<header>
+  <div class="masthead">
+    <a href="/">
+      <img src="/images/logo.svg" alt="Sunnyside - Home" class="logo">
+    </a>
+    <Nav/>
+  </div>
+  <div class="hero">
+    <h1>We are creatives</h1>
+    <img src="/images/icon-arrow-down.svg" alt="">
+  </div>
+</header>
+
+<style>
+  header {
+    background-color:#3ebfff;
+    background-image: url('/images/mobile/image-header.jpg');
+    background-position: bottom center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding: 2rem 1.5rem;
+    min-height: 33.625rem;
+    position: relative;
+  }
+
+  .masthead {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+  }
+
+  .hero {
+    text-align: center;
+  }
+
+  h1 {
+    margin-top: 5.25rem;
+    font-weight: 900;
+    font-size: 2.5rem;
+    letter-spacing: 0.3969rem;
+    color: var(--color-white);
+    text-transform: uppercase;
+  }
+
+  .hero img {
+    margin-top: 3rem;
+  }
+
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,76 @@
+<script>
+  let active = false;
+  function toggleMenu(state) {
+    active = typeof state === 'boolean' ? state : !active;
+  }
+
+</script>
+
+<nav>
+  <button aria-label="menu toggle" aria-expanded={active} class:open={active} on:click={toggleMenu}>
+    <img src="/images/icon-hamburger.svg" alt="">
+  </button>
+  <ul class="menu" class:closed={!active}>
+    <li class="menu-item"><a href="/">About</a></li>
+    <li class="menu-item"><a href="/">Services</a></li>
+    <li class="menu-item"><a href="/">Projects</a></li>
+    <li class="menu-contact"><a href="/">Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  button {
+    transition: opacity .4s ease;
+  }
+
+  .open {
+    opacity: 0.6;
+  }
+
+  .closed {
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  .menu {
+    position: absolute;
+    top: 74px;
+    right: 0;
+    width: 100%;
+    max-width: 500px;
+    display: grid;
+    justify-items: center;
+    grid-gap: 2rem;
+    padding: 2.5rem 0;
+    background-color: var(--color-white);
+    transition: all .3s ease;
+  }
+
+  .menu::before {
+    position: absolute;
+    content: "";
+    top: -1.5rem;
+    right: 0;
+    border: .75rem solid;
+    border-color: transparent var(--color-white) var(--color-white)transparent;
+  }
+
+  .menu-item {
+    color: var(--color-gray-blue);
+    font-weight: 500;
+    font-size: 1.25rem;
+    line-height: 1.25;
+  }
+
+  .menu-contact {
+    color: var(--color-dark-blue);
+    text-transform: uppercase;
+    font-family: "Fraunces", serif;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 1.667;
+    background-color: var(--color-yellow);
+    border-radius: 2rem;
+    padding: 16px 32px;
+  } 
+</style>
